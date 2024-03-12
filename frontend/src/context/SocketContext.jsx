a3c1837baf1d933ff711e142eb78e746b28d93f2
Benny Lan <lanbenny6@gmail.com>
2024-03-12 00:20:01 -0400

    update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://mernchat-e3sx.onrender.com", {
+			const socket = io("http://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},
